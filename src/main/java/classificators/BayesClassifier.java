2aba0d5e656013beb1b8801bec14e408066c1b0e
bizin <dmitry.bizin@magenta-technology.com>
2018-09-21 10:19:00 +0400

    заготовка для байесовского классификатора

--- src/main/java/classificators/BayesClassifier.java
+++ src/main/java/classificators/BayesClassifier.java
@@ -6,12 +6,14 @@ import java.util.Map;
 public abstract class BayesClassifier {
 
     //http://synergy-journal.ru/archive/article1737
+    //http://bazhenov.me/blog/2012/06/11/naive-bayes
 
     public static void train(Map<Class, List<String>> classToKeywordsMap) {
 
     }
 
     public static Class classify(List<String> keywords) {
+
 
     }
 
